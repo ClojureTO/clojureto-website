804d9828b2e8abb49dc3550613f551e8a780c081
Hildeberto Mendonca <hmendonca@cta.loc>
2019-05-13 21:44:10 -0400

    Clojure Books in the Toronto Public Library

--- project.clj
+++ project.clj
@@ -8,8 +8,8 @@
                            [compojure "1.6.1"]
                            [ring-server "0.5.0"]
                            [cryogen-markdown "0.1.7"]
-                           [cryogen-core "0.1.65"]]
-            :plugins [[lein-ring "0.9.7"]]
+                           [cryogen-core "0.1.66"]]
+            :plugins [[lein-ring "0.12.5"]]
             :main cryogen.core
             :ring {:init cryogen.server/init
                    :handler cryogen.server/handler})
